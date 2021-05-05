443a2e2fa1d351ae6c18c191b4a37bac3398c2e6
FelipeMares <78992149+FelipeMares@users.noreply.github.com>
2021-05-04 21:15:10 -0500

    Add files via upload

---
+++ Tareae3 Script PS.ps1
@@ -0,0 +1,10 @@
+﻿$Path = "C:\Users\LENOVO\Libros"
+"{0,10} {1,-24} {2,-2}" -f `
+" Size", "Last Accessed", "File Name "
+Foreach ($file in Get-Childitem $Path -recurse -force)
+{If ($file.extension -eq ".txt")
+    {
+    "{0,10} {1,-24} {2,-2}" -f `
+    $file.length, $file.LastAccessTime, $file.fullname
+    }
+}
